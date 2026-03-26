34e2044990e443f431fe3e493125e8e4093b37cb
Milan Vala <valamilan2005@gmail.com>
2026-03-26 10:47:45 +0530

    created customeraccount class file

---
+++ Practical-2/CustomerAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace BankAccount;
+
+class CustomerAccount
+{
+    public static string? bank_name;
+    public long customer_accountNo;
+    public string customer_name;
+
+    public CustomerAccount(long accountNo, string customerName)
+    {
+        customer_accountNo = accountNo;
+        customer_name = customerName;
+    }
+
+    /// <summary>
+    /// Purpose: Displays all details of the customer account including bank name, account number, and customer name.
+    /// Return Type: void (does not return any value)
+    /// </summary>
+    /// 
+    public void printInfo()
+    {
+        Console.WriteLine("Customer Account Details");
+        Console.WriteLine("Bank Name: " + bank_name);
+        Console.WriteLine("Account Number: " + customer_accountNo);
+        Console.WriteLine("Customer Name: " + customer_name);
+    }
+}
--- Practical-2/Program.cs
+++ Practical-2/Program.cs
@@ -1,38 +1,15 @@
 ﻿namespace BankAccount;
 
-class CustomerAccount
-{
-    public string bank_name;
-    public long customer_accountNo;
-    public string customer_name;
-
-    public CustomerAccount(long accountNo, string customerName)
-    {
-        customer_accountNo = accountNo;
-        customer_name = customerName;
-    }
-
-    /// <summary>
-    /// Purpose: Displays all details of the customer account including bank name, account number, and customer name.
-    /// Return Type: void (does not return any value)
-    /// </summary>
-    public void printInfo()
-    {
-        Console.WriteLine("Customer Account Details");
-        Console.WriteLine("Bank Name: " + bank_name);
-        Console.WriteLine("Account Number: " + customer_accountNo);
-        Console.WriteLine("Customer Name: " + customer_name);
-    }
-}
-
 class Program
 {
     static void Main()
     {
-        CustomerAccount customer = new CustomerAccount(9876543210, "Milan Vala");
+        CustomerAccount c = new CustomerAccount(9876543210, "Milan Vala");
 
-        customer.bank_name = "Bank of Baroda";
+        //Initializing static variable using the class name
+        CustomerAccount.bank_name = "Bank of Baroda";
 
-        customer.printInfo();
+        //prints the info of Customer account 
+        c.printInfo();
     }
 }
